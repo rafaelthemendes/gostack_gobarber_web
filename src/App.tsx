ed05f0f81acb7eb7498b3445dc2e595da8aafec4
Rafael Mendes <rafaelthemendes@gmail.com>
2020-05-22 00:19:16 -0300

    :lipstick: styled components, global styles and font roboto slab

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
 
-function App() {
-  return <h1>Faaala dev!</h1>
-}
+const App: React.FC = () => (
+  <>
+    <h1>Faaala dev!</h1>
+    <GlobalStyles />
+  </>
+);
 
 export default App;
